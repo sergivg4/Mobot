9fb1e2fecdf2fa6f5a1ed04f06d0f0b94e23ee20
RogerReyesIso <roger.kings8@gmail.com>
2022-05-06 16:51:36 +0200

    login con el brillito

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -1,37 +1,210 @@
+<style>
+.mobotTitle {
+  font-size: 35px;
+  margin-left: 10%;
+  margin-top: 1%;
+  color: #ffffff;
+}
+
+body {
+  display: flex;
+  margin: 0px;
+  font-family: sans-serif;
+}
+
+.login-box {
+  position: absolute;
+  top: 50%;
+  left: 70%;
+  width: 400px;
+  padding: 40px;
+  transform: translate(-50%, -50%);
+  background: rgba(0, 0, 0, 0.5);
+  box-sizing: border-box;
+  box-shadow: 0 15px 25px rgba(0, 0, 0, 0.6);
+  border-radius: 10px;
+}
+
+.login-box h2 {
+  margin: 0 0 30px;
+  padding: 0;
+  color: #fff;
+  text-align: center;
+}
+
+.login-box .user-box {
+  position: relative;
+}
+
+.login-box .user-box input {
+  width: 100%;
+  padding: 10px 0;
+  font-size: 16px;
+  color: #fff;
+  margin-bottom: 30px;
+  border: none;
+  border-bottom: 1px solid #fff;
+  outline: none;
+  background: transparent;
+}
+.login-box .user-box label {
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 10px 0;
+  font-size: 16px;
+  color: #fff;
+  pointer-events: none;
+  transition: 0.5s;
+}
+
+.login-box .user-box input:focus ~ label,
+.login-box .user-box input:valid ~ label {
+  top: -20px;
+  left: 0;
+  color: #05e2e2;
+  font-size: 12px;
+}
+
+.login-box form a {
+  position: relative;
+  display: inline-block;
+  padding: 10px 20px;
+  color: #05e2e2;
+  font-size: 16px;
+  text-decoration: none;
+  text-transform: uppercase;
+  overflow: hidden;
+  transition: 0.5s;
+  margin-top: 40px;
+  letter-spacing: 4px;
+}
+
+.login-box a:hover {
+  background: #05e2e2;
+  color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 0 2px #05e2e2, 0 0 10px #05e2e2, 0 0 20px #05e2e2,
+    0 0 40px #05e2e2;
+}
+
+.login-box a span {
+  position: absolute;
+  display: block;
+}
+
+.login-box a span:nth-child(1) {
+  top: 0;
+  left: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(90deg, transparent, #05e2e2);
+  animation: btn-anim1 5s linear infinite;
+}
+
+@keyframes btn-anim1 {
+  0% {
+    left: -100%;
+  }
+  50%,
+  100% {
+    left: 100%;
+  }
+}
+
+.login-box a span:nth-child(2) {
+  top: -100%;
+  right: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(180deg, transparent, #05e2e2);
+  animation: btn-anim2 5s linear infinite;
+  animation-delay: 1.25s;
+}
+
+@keyframes btn-anim2 {
+  0% {
+    top: -100%;
+  }
+  50%,
+  100% {
+    top: 100%;
+  }
+}
+
+.login-box a span:nth-child(3) {
+  bottom: 0;
+  right: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(270deg, transparent, #05e2e2);
+  animation: btn-anim3 5s linear infinite;
+  animation-delay: 2.5s;
+}
+
+@keyframes btn-anim3 {
+  0% {
+    right: -100%;
+  }
+  50%,
+  100% {
+    right: 100%;
+  }
+}
+
+.login-box a span:nth-child(4) {
+  bottom: -100%;
+  left: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(360deg, transparent, #05e2e2);
+  animation: btn-anim4 5s linear infinite;
+  animation-delay: 3.75s;
+}
+
+@keyframes btn-anim4 {
+  0% {
+    bottom: -100%;
+  }
+  50%,
+  100% {
+    bottom: 100%;
+  }
+}
+</style>
 <template>
   <div id="Login">
-      Login
+    <div class="mobotTitle">MOBOT</div>
+    <div class="login-box">
+      <h2>Login</h2>
+      <form>
+        <div class="user-box">
+          <input type="text" name="" required="" />
+          <label>Username</label>
+        </div>
+        <div class="user-box">
+          <input type="password" name="" required="" />
+          <label>Password</label>
+        </div>
+        <a href="#">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Submit
+        </a>
+      </form>
+    </div>
   </div>
 </template>
-<style>
-  @import '../styles/app.css';
-</style>
+
 <script>
-  import axios from "../helpers/axios_import";
-/*   import 'vue-loading-overlay/dist/vue-loading.css';
- */  
+import axios from "../helpers/axios_import";
+
 export default {
-    data: function () {
-      return {
-
-      }
-    },
-    methods:{
-    },
-    mounted() {
-      axios.interceptors.response.use((response) => {
-          return response;
-      }, (error) => {
-          if(error.response.status === 401) {
-              localStorage.clear();
-              this.$router.push('/')
-          }
-          return error;
-      });
-      if (localStorage.getItem('userData')) {
-        this.userData = JSON.parse(localStorage.getItem('userData'));
-      }
-    }
-  }
-
+  data: function () {
+    return {};
+  },
+  methods: {},
+};
 </script>
