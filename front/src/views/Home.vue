c16c6b8b2d0b0ad96d5946c377c27b7f6c80f064
maqueru <92113124+maqueru@users.noreply.github.com>
2022-05-24 18:47:05 +0200

    Home-Mobile

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,52 +1,54 @@
 <style>
  /* MOBILE HOME CSS */
-#home {background: #191A1F;background: linear-gradient(#191A1F 50%, rgba(7,6,93,1) 73%, rgba(16,16,185,1) 104%);
-  display: flex;height: 100%;width: 100%;flex-flow: column;/* background: linear-gradient(#191A1F, #0754F1 175%); */}
-#home-header {height: 15vh;width: 100%; }
-#home-arm-container {height: 30rem;width: 100%;}
-#home-bottom {width: 100%;border-top: 4px solid white;height: 18rem;}
-.bot-title {color: #cdb04e;text-shadow: 0px 0px 2px #493902;font-weight: 600;}
-.mo-title {color: #d0d0d0;}
-.home-title {display: flex;padding: 25px;font-size: 1.5rem;}
-.mo-title,.bot-title{line-height: 50px; letter-spacing: 1px;text-decoration: 2px underline;}
-
+#home {background: #191A1F;background:linear-gradient(#191A1F 57%, rgba(7,6,93,1) 104%, rgba(16,16,185,1) 81%);display: flex;height: 100%;width: 100%;flex-flow: column;/* background: linear-gradient(#191A1F, #0754F1 175%); */}
+#home-header {height: 15vh;width: 100%; display: flex; flex-flow: row; align-items: center;}
+#home-arm-container {height: 24rem;width: 100%;}
+#home-bottom {width: 100%;height: 18rem;display: flex;align-items: center;flex-flow: column;}
+.home-bottom-header {padding-bottom: 15px;padding-top: 5px;font-size: 9px;color: white;letter-spacing: 2px;width: 19rem;}
+.home-bottom-body {padding-left: 15px;padding-right: 15px;position: relative;width: 19rem;}
+.home-bottom-body p{font-size: 15px;color: #cbcbcb;letter-spacing: 2px;}
+.home-bottom-body span{cursor:pointer; border-bottom: 1px solid rgb(255, 255, 255); padding-bottom:0.2px;font-size: 15px;color: #cbcbcb;letter-spacing: 2px;margin-top: 15px;position: absolute;}
+.home-title {padding-left: 30px;padding-top: 5px;color: rgb(255,255,255,1.5);}
 </style>
 <template>
   <div id="home">
     <div id="home-header">
       <div class="home-title">
-        <h1 class="mo-title">MO</h1>
-        <h1 class="bot-title">BOT</h1>
+        <h1>MOOBOT</h1>
+      </div>
+      <div class="mobile-nav-trigger">
+        <!-- ICON TO DROP DOWN THE THE MENU --> 
       </div>
     </div>
     <div id="home-arm-container">
       <ArmModel></ArmModel>
     </div>
     <div id="home-bottom">
-
+      <div class="home-bottom-header">
+        <h1>MOVE YOUR ROBOT EASIER</h1>
+      </div>
+      <div class="home-bottom-body">
+        <p>Our app was developed with the idea of make in 
+        easy the fact of move your robot arm</p>
+        <span>More info</span>
+      </div>
     </div>
     <div id="home-info">
-
+      
     </div>
-
-        <h1>Home Page</h1>
   </div>
 </template>
 
 <script>
-  import axios from "../helpers/axios_import";
-  import ArmModel from './ArmModel.vue';
+import axios from "../helpers/axios_import";
+import ArmModel from "./ArmModel.vue";
 
 export default {
   components: { ArmModel },
-    name:"Home",
-    data: function () {
-      return {
-
-      }
-    },
-    methods:{
-    },
-  }
-
+  name: "Home",
+  data: function () {
+    return {};
+  },
+  methods: {},
+};
 </script>
